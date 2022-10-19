283f5a88e23a1a92c2568a604074a547e34452cb
Rishiraj Aman <78068099+hellzom@users.noreply.github.com>
2022-10-19 17:00:49 +0530

    Time: 248 ms (70.34%), Space: 10.3 MB (28.25%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=2;i<n;i++){
             if(v[i]){
                 count++;
-                for(int j=i*2;j<n;j=j+i){
+                for(int j=i*2;j<n;j+=i){
                     v[j] = false;
                 }
             }
